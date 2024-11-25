744407aa957e9da6de439e3705cbd28f10f2cc0c
Pablo Friedmann <pabloifm91@gmail.com>
2024-11-25 21:36:41 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+const [tarea, miTarea] = useState ("");
+const [tareas, listaTareas] = useState([]);
+
+const agregarTarea = (event) => {
+    if (event.key === "Enter" && tarea !== "") {
+        listaTareas(tareas.concat(tarea));
+        miTarea("");
+    }
+};
+
+const borrarTarea = (index) => {
+    const nuevasTareas = tareas.filter((t, i) => i !== index);
+    listaTareas(nuevasTareas);
+};
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+			<h1 className="text-center">To do list</h1>
+			<div>
+				<input type="text" placeholder="What needs to be done?" value= {tarea} onChange= {(event)=> miTarea (event.target.value)} onKeyDown={agregarTarea} />
+			</div>
+			<ul>
+                {tareas.map((t, index) => (
+                    <li key={index}>
+                        {t}
+                        <span>
+                            <button onClick={() => borrarTarea(index)}>x</button>
+                        </span>
+                    </li>
+                ))}
+            </ul>
+			<div >
+        <small>
+		{tareas.length} item left
+		</small>
+			</div>
+        </div>
+    );
 };
 
 export default Home;
